c852e5bd255ccd0744bc416e096f0ecc7caf88f7
Роман <dudoitsrv@gmail.com>
2020-06-15 22:08:24 +0300

    4.2. Тернарное сравнение [#306506]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -2,9 +2,7 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int first, int second) {
-        boolean can = first >= second;
-        int result = can ? first : second;
-        return result;
+        return  first >= second ? first : second;
     }
 
 
