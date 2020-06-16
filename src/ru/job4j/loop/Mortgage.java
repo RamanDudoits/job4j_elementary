a704efc2e3976e92075ed74ef9944da5584eb273
Роман <dudoitsrv@gmail.com>
2020-06-16 16:24:15 +0300

    5.7. Ипотека [#306594]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,19 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public int year(double amount, int salary, double percent) {
+        int year = 0;
+        double balance;
+        while (amount < salary) {
+            balance = amount * percent / 100;
+            amount = amount + balance;
+          amount = amount - salary;
+          if (amount < 0) {
+              year++;
+              break;
+          }
+          year++;
+        }
+        return year;
+    }
+}
