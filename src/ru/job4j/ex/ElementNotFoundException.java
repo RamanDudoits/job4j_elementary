215da37eed713bac7240329bcf49fa698b930483
Роман <dudoitsrv@gmail.com>
2020-07-09 19:40:46 +0300

    0.4. Пользовательские исключения. [#306662]

--- src/ru/job4j/ex/ElementNotFoundException.java
+++ src/ru/job4j/ex/ElementNotFoundException.java
@@ -8,11 +8,13 @@ public class ElementNotFoundException extends Exception {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
         for (int i = 0; i < value.length; i++) {
-            if (value[i] == key) {
+            if (value[i].equals(key)) {
                 rsl = i;
-            } else {
-                throw new ElementNotFoundException("Not this element");
+                break;
             }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Not this element");
         }
         return rsl;
     }
